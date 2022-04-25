757b8cab41c01033f14a3fb807be400ae7906a71
isaacmain254 <isaacmain72@gmail.com>
2022-04-25 17:47:59 +0300

    last commit

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import './Drink.css';
+import React from "react";
+import "./Drink.css";
 
-const Drink = ({drink}) => (
-    <div className="card-item">
-        <div className="card-content">
-            <img src={drink.image} alt="coffee image" />
-            <p className='price'>${drink.price}</p>
-            <h2>{drink.name}</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, placeat?</p>
-            <button>Get Delivery</button>
-
-        </div>
+const Drink = ({ drink }) => (
+  <div className="card-item">
+    <div className="card-content">
+      <img src={drink.image} alt="coffeeimage" />
+      <p className="price">${drink.price}</p>
+      <h2>{drink.name}</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore,
+        placeat?
+      </p>
+      <button>Get Delivery</button>
     </div>
-)
+  </div>
+);
 
 export default Drink;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 const Home = () => {
   return (
-    <div className='home' id='home'>
+    <div className="home" id="home">
       <div className="home-content">
         <div className="left-content">
-          <p className="first-p">What's life Without <span>coffee?</span></p>
+          <p className="first-p">
+            What's life Without <span>coffee?</span>
+          </p>
           <p className="second-p">Welcome!</p>
           <p className="third-p">We serve the richest coffee in the city!</p>
-          <p className="fourth-p">High quality in every cup of coffee that we serve ...</p>
-          <button>Order Now</button>
+          <p className="fourth-p">
+            High quality in every cup of coffee that we serve ...
+          </p>
+          <button className="order">Order Now</button>
         </div>
         <div className="right-content">
           <img src="images/coffee7-nobg.png" alt="coffee cup" />
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
