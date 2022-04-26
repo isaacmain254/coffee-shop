c15fde17e0f6739083f0f52154332f3fb5a8754d
isaacmain254 <isaacmain72@gmail.com>
2022-04-26 17:37:10 +0300

    last commit

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,27 +1,32 @@
 import React from "react";
-import { FaAngleRight } from 'react-icons/fa';
-import './Shop.css';
+import { FaAngleRight } from "react-icons/fa";
+import "./Shop.css";
 
 const Shop = () => {
   return (
-    <div className="shop-content">
+    <div className="shop-content" id="shop">
       <div className="shop">
-      <div className="shop-left">
-        <h2>Your home deserves coffee ,too</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-          recusandae sed tempore beatae quam dolore tempora iusto architecto
-          ullam quia fugit, voluptates amet quibusdam expedita distinctio autem
-          excepturi. Porro, esse facilis sequi, debitis illo aut repellendus
-          ipsum error nemo ratione ad pariatur itaque distinctio laboriosam?
-        </p>
-        
+        <div className="shop-left">
+          <h2>Your home deserves coffee ,too</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+            recusandae sed tempore beatae quam dolore tempora iusto architecto
+            ullam quia fugit, voluptates amet quibusdam expedita distinctio
+            autem excepturi. Porro, esse facilis sequi, debitis illo aut
+            repellendus ipsum error nemo ratione ad pariatur itaque distinctio
+            laboriosam?
+          </p>
+        </div>
+        <div className="shop-right">
+          <img src="images/coffee6-nobg.png" alt="coffee" />
+        </div>
       </div>
-      <div className="shop-right">
-        <img src="images/coffee6-nobg.png" alt="coffee"/>
-      </div>
-      </div>
-      <p className="shop-now"><a href="/shop">More Coffee<FaAngleRight/></a></p>
+      <p className="shop-now">
+        <a href="/shop">
+          More Coffee
+          <FaAngleRight />
+        </a>
+      </p>
     </div>
   );
 };
